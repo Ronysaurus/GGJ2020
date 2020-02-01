682e4ac58618f46be1a17b750ed99776f6944321
Sulejo9 <larasebastian55@hotmail.com>
2020-02-01 14:40:47 -0600

    Script enemigo follow, menu, y mas
    Agregado el script enemigo, menu, y sprite

---
+++ Assets/Sulejo/Enemigo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+
+	public Transform ObjectToFollow = null;
+	public float Speed =2;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        ObjectToFollow = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ObjectToFollow == null)
+	return;
+
+	transform.position = Vector2.MoveTowards(transform.position, ObjectToFollow.transform.position, Speed *Time.deltaTime);
+	transform.up = ObjectToFollow.position - transform.position ;
+    }
+}
