b87422e1dd306c4cb97fad3fa3743c782dd51c89
Sulejo9 <larasebastian55@hotmail.com>
2020-02-01 20:11:46 -0600

    Sphere following target
    Sphere follows target

---
+++ Assets/Sulejo/FollowSight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowSight : MonoBehaviour
+{
+	
+	public Transform theTarget;
+	
+	//
+	public float RotSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.LookAt(theTarget);
+    }
+}
