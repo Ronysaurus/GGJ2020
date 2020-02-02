d55f17d1f7e907ab5cbb1daf3298188f45aa0288
Alain <barraganaalin55@gmail.com>
2020-02-01 20:20:53 -0600

    merge animations player

---
+++ Assets/Alain/Scripts/Scr_PlayerControl.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class Scr_PlayerControl : MonoBehaviour
+{
+    private Animator anim;
+    private float atackCD;
+    private Scr_PlayerStats stats;
+    private int running;
+
+    private void Start()
+    {
+        stats = this.GetComponent<Scr_PlayerStats>();
+        atackCD = 1.2f;
+        anim = this.GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+        atackCD -= Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Atack();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Repair();
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            anim.SetBool("Repair", false);
+        }
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical") * running);
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+    }
+
+    private void Move()
+    {
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * 50);
+        running = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
+        this.transform.Translate(this.transform.forward * stats.GetSpeed() * Time.deltaTime * Input.GetAxis("Vertical") * running);
+        if (Input.GetAxis("Vertical") > 0)
+            return;
+        this.transform.Translate(this.transform.right * stats.GetSpeed() * Time.deltaTime * Input.GetAxis("Horizontal"));
+    }
+    private void Atack()
+    {
+        if (atackCD > 0)
+            return;
+        atackCD = 1.2f;
+        PerformAction("Atack");
+    }
+
+    private void Repair()
+    {
+        PerformAction("Repair");
+    }
+
+    private void Dance()
+    {
+        PerformAction("Dance");
+    }
+
+    private void Death()
+    {
+        PerformAction("Die");
+    }
+
+    private void PerformAction(string action)
+    {
+        anim.SetTrigger(action);
+    }
+}
---
+++ Assets/Alain/uniat-chan/source/Scr_PlayerStats.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class Scr_PlayerStats : MonoBehaviour
+{
+    [SerializeField]
+    private int hp;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private int maxHp;
+
+    [SerializeField]
+    private float atk;
+
+    [SerializeField]
+    private int gold;
+
+    public int getGold()
+    {
+        return gold;
+    }
+
+    public void SetGold(int _gold)
+    {
+        gold = _gold;
+    }
+
+    public float GetAtack()
+    {
+        return atk;
+    }
+
+    public void SetAtack(float _atk)
+    {
+        atk = _atk;
+    }
+
+    public int GetMaxHp()
+    {
+        return maxHp;
+    }
+
+    public void SetMaxHP(int _maxHP)
+    {
+        maxHp = _maxHP;
+    }
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    public void SetSpeed(float _speed)
+    {
+        speed = _speed;
+    }
+
+    public int GetHP()
+    {
+        return hp;
+    }
+
+    public void SetHp(int _hp)
+    {
+        hp = _hp;
+    }
+}
